8e58d7fa9a34d7a20ed96abdd463a3cf1129ce81
losofy <60601742+losofy@users.noreply.github.com>
2023-02-11 17:49:55 +0100

    Message success a l'ajout d'un mot

--- sutomProject/sutomGame/views/mot/create.py
+++ sutomProject/sutomGame/views/mot/create.py
@@ -1,3 +1,5 @@
+from django.contrib import messages
+from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views.generic import CreateView
 
@@ -11,3 +13,9 @@ class MotCreateView(CreateView):
 
     def get_success_url(self):
         return reverse('admin_list_mot')
+
+    def post(self, request, *args, **kwargs):
+        print("TEST")
+        messages.all_messages = []
+        messages.add_message(self.request, messages.SUCCESS, 'Nouveau mot créé avec succès !')
+        return HttpResponseRedirect(reverse('admin_list_mot'))
